c478336da228cdf6dcba8077083cae93b4b8388d
Latifa Jarrah <latifa.200100@gmail.com>
2025-03-19 15:19:35 +0200

    modify Photo component for improved layout and centering in Developer template

--- src/app/components/templates/resturant/ResturantNavbar.tsx
+++ src/app/components/templates/resturant/ResturantNavbar.tsx
@@ -7,7 +7,7 @@ export default function ResturantNabvbar() {
   return (
     <nav
       // className="fixed w-full bg-black bg-opacity-20 py-4 px-6 flex justify-between items-center z-50"
-      className="w-full bg-black bg-opacity-20 py-4 px-6 flex justify-between items-center z-50"
+      className="w-full bg-black bg-opacity-20 py-4 px-6 flex justify-between items-center z-50 fixed top-0 left-0 right-0"
     >
       <ResturantLogo />
       <NavbarList />
--- src/components/ui/Photo.jsx
+++ src/components/ui/Photo.jsx
@@ -5,7 +5,8 @@ import Image from "next/image";
 
 const Photo = () => {
   return (
-    <div className="w-full h-full relative">
+    // <div className="w-full h-full relative">
+    <div className="w-full h-full relative flex items-center justify-center">
       <motion.div
         initial={{ opacity: 0 }}
         animate={{
@@ -20,7 +21,8 @@ const Photo = () => {
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: "easeOut" },
           }}
-          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
+          // className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
+          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 mix-blend-lighten"
         >
           <Image
             src="/assets/photo.png"
@@ -33,7 +35,7 @@ const Photo = () => {
         </motion.div>
         {/* circule*/}
         <motion.svg
-          className="w-[300px] xl:[506px] h-[300px] xl:h-[506px]"
+          className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
           fill="transparent"
           viewBox="0 0 506 506"
           xmlns="http://www.w3.org/2000/svg"
