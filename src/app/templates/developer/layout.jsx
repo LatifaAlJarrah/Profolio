492deb8caec4e0cda1875cf924d39641dbf2ce8c
Latifa Jarrah <latifa.200100@gmail.com>
2025-03-20 11:01:30 +0200

    Refactor Developer template: replace TypeScript files with JavaScript versions for layout and page components

--- src/app/templates/developer/layout.jsx
+++ src/app/templates/developer/layout.jsx
@@ -2,13 +2,7 @@ import Header from "@/app/components/templates/developer/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/StairTransition";
 
-import { ReactNode } from "react";
-
-interface LayoutProps {
-  children: ReactNode;
-}
-
-export default function Layout({ children }: LayoutProps) {
+export default function Layout({ children }) {
   return (
     <html lang="en">
       <body className="bg-primarydev text-white font-jetBrainsMono leading-loose px-48">
