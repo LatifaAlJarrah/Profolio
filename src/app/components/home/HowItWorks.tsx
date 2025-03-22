e184dbde72c30ff0355bc2c95712b5dcba6a2be3
Latifa Jarrah <latifa.200100@gmail.com>
2025-03-22 12:55:17 +0200

    Refactor BlogSection and HowItWorks components for improved layout; reduce padding in BlogSection and clean up HowItWorks code

--- src/app/components/home/HowItWorks.tsx
+++ src/app/components/home/HowItWorks.tsx
@@ -1,45 +1,3 @@
-// import React from "react";
-// import { Website, WebsiteBuilder, ContentCreator } from "@assets/images";
-// import Image from "next/image";
-// import Steps from "./Steps";
-
-// const features = [
-//   { icon: Website, title: "Choose A Template" },
-//   { icon: ContentCreator, title: "Customize The Template" },
-//   { icon: WebsiteBuilder, title: "Launch Your Website" },
-// ];
-
-// export default function HowItWorks() {
-//   return (
-//     <section className="text-center py-10 px-20">
-//       <h2 className="text-2xl font-bold mb-6">How It Works ?</h2>
-//       <div className="bg-gray-200 p-6 rounded-lg mx-auto w-full md:w-3/4 mb-12 md:mb-20">
-//         <button className="bg-gray-400 w-16 h-16 rounded-lg">▶</button>
-//       </div>
-//       <div className="flex flex-col md:flex-row justify-around space-y-8 md:space-y-0 md:space-x-6">
-//         {features.map((feature, index) => (
-//           <React.Fragment key={index}>
-//             <div className="bg-white p-6 md:p-8 rounded-2xl shadow-md text-center relative overflow-visible">
-//               <div className="flex justify-center">
-//                 <Image
-//                   src={feature.icon}
-//                   alt={feature.title}
-//                   width={130}
-//                   height={130}
-//                   className="-mt-28 -ml-52"
-//                 />
-//               </div>
-//               <p className="mt-10 text-lg md:text-xl text-black leading-5 font-normal">
-//                 {feature.title}
-//               </p>
-//             </div>
-//             {index !== 2 && <Steps />}
-//           </React.Fragment>
-//         ))}
-//       </div>
-//     </section>
-//   );
-// }
 import React from "react";
 import Image from "next/image";
 import { Website, WebsiteBuilder, ContentCreator } from "@assets/images";
@@ -47,7 +5,6 @@ import Steps from "./Steps";
 
 import { StaticImageData } from "next/image";
 
-// Feature data
 const features = [
   { icon: Website, title: "Choose A Template" },
   { icon: ContentCreator, title: "Customize The Template" },
