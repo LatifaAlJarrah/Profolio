5ccdc8428965032ea28fb5ff3cfdc588ca0133df
Latifa Jarrah <latifa.200100@gmail.com>
2025-05-05 15:10:05 +0300

    update ControlTemplate for improved rendering and add default template data

---
+++ src/app/components/data/defaultTemplateDeveloperData.ts
@@ -0,0 +1,8 @@
+import { TemplateData } from "@/app/types/templateData";
+
+export const defaultTemplateDeveloperData: TemplateData = {
+  navbarColor: "#1c1c22",
+  fontFamily: "font-jetBrainsMono",
+  logoName: "Luke",
+  logoColor: "#ffffff",
+};
