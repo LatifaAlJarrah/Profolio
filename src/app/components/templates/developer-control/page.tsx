43f1cd75c3e3d1f275d8127c60f37a9a0d51ff31
Latifa Jarrah <latifa.200100@gmail.com>
2025-05-08 15:18:49 +0300

    Refactor TemplatePreview by improving imports

--- src/app/components/templates/developer-control/page.tsx
+++ src/app/components/templates/developer-control/page.tsx
@@ -1,126 +1,3 @@
-// "use client";
-// import * as React from "react";
-// import Header from "../developer/Header";
-// import Home from "@/app/templates/developer/page";
-// import Work from "@/app/templates/developer/work/page";
-// import Services from "@/app/templates/developer/services/page";
-// import Resume from "@/app/templates/developer/resume/page";
-// import Contact from "@/app/templates/developer/contact/page";
-// import { TemplateData } from "@/app/types/templateData";
-
-// interface TemplatePreviewProps extends TemplateData {
-//   backgroundColor?: string;
-//   logoName?: string;
-//   logoColor?: string;
-//   navigationLinks?: Array<{ name: string; link: string }>;
-//   navigationLinksColor?: string;
-//   navbarColor?: string;
-//   hireMeButton?: { text: string; path: string; color: string };
-//   developerHeaderRole?: string;
-//   developerHeadertitle?: string;
-//   developerHeadername?: string;
-//   developerHeaderdescription?: string;
-//   developerHeaderImage?: string;
-//   developerHeaderServicesButtonColor?: string;
-//   developerHeaderServicesButtonText?: string;
-//   developerInstagramLink?: string;
-//   developerLinkedinLink?: string;
-//   developerTwitterLink?: string;
-//   ourAchievements?: Array<{
-//     number?: number;
-//     text?: string;
-//   }>;
-//   developerProjects?: Array<{
-//     num: string;
-//     category: string;
-//     description: string;
-//     stack: Array<{ name: string }>;
-//     image: string;
-//     live: string;
-//     github: string;
-//   }>;
-//   developerServices?: Array<{
-//     num?: string;
-//     title?: string;
-//     description?: string;
-//     href?: string;
-//   }>;
-// }
-
-// const TemplatePreview = ({
-//   backgroundColor,
-//   logoName,
-//   logoColor,
-//   navigationLinks,
-//   navigationLinksColor,
-//   navbarColor,
-//   hireMeButton,
-//   developerHeaderRole,
-//   developerHeadertitle,
-//   developerHeadername,
-//   developerHeaderdescription,
-//   developerHeaderImage,
-//   developerHeaderServicesButtonText,
-//   developerHeaderServicesButtonColor,
-//   developerInstagramLink,
-//   developerLinkedinLink,
-//   developerTwitterLink,
-//   ourAchievements,
-//   developerProjects,
-//   developerServices,
-// }: TemplatePreviewProps) => {
-//   return (
-//     <div className="bg-primarydev text-white font-jetBrainsMono leading-loose px-20 py-12">
-//       <div className="mb-12">
-//         <Header
-//           logoName={logoName}
-//           logoColor={logoColor}
-//           navigationLinks={navigationLinks}
-//           navigationLinksColor={navigationLinksColor}
-//           navbarColor={navbarColor}
-//           hireMeButton={hireMeButton}
-//         />
-//       </div>
-
-//       <div className="mb-12">
-//         <Home
-//           backgroundColor={backgroundColor}
-//           developerHeaderRole={developerHeaderRole}
-//           developerHeadertitle={developerHeadertitle}
-//           developerHeadername={developerHeadername}
-//           developerHeaderdescription={developerHeaderdescription}
-//           developerHeaderImage={developerHeaderImage}
-//           developerHeaderServicesButtonText={developerHeaderServicesButtonText}
-//           developerHeaderServicesButtonColor={
-//             developerHeaderServicesButtonColor
-//           }
-//           developerInstagramLink={developerInstagramLink}
-//           developerLinkedinLink={developerLinkedinLink}
-//           developerTwitterLink={developerTwitterLink}
-//           ourAchievements={ourAchievements}
-//         />
-//       </div>
-
-//       <div className="mb-12">
-//         <Work developerProjects={developerProjects} />
-//       </div>
-
-//       <div className="mb-12">
-//         <Services developerServices={developerServices } />
-//       </div>
-
-//       <div className="mb-12">
-//         <Resume />
-//       </div>
-
-//       <div className="mb-12">
-//         <Contact />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default TemplatePreview;
 "use client";
 
 import React from "react";
@@ -132,7 +9,6 @@ import Resume from "@/app/templates/developer/resume/page";
 import Contact from "@/app/templates/developer/contact/page";
 import { TemplateData } from "@/app/types/templateData";
 
-// تعريف واجهة الخاصية مع السماح بقيم اختيارية بشكل صريح
 interface TemplatePreviewProps extends TemplateData {
   backgroundColor?: string;
   logoName?: string;
@@ -172,7 +48,6 @@ interface TemplatePreviewProps extends TemplateData {
   }>;
 }
 
-// تعريف الصفحة كمكون افتراضي
 const TemplatePreview: React.FC<TemplatePreviewProps> = ({
   backgroundColor,
   logoName,
