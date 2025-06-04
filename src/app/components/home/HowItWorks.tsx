9c06e238122bd7a25c964d9bc90d54b75b7b7f59
Latifa Jarrah <latifa.200100@gmail.com>
2025-06-04 12:34:26 +0300

    .

--- src/app/components/home/HowItWorks.tsx
+++ src/app/components/home/HowItWorks.tsx
@@ -34,7 +34,7 @@ export default function HowItWorks() {
         <div className="flex justify-center my-5">
           <div className="bg-[#F3F3F3] w-full lg:w-1/2 h-[150px] lg:h-[300px] flex items-center justify-center rounded-md mb-10">
             <video className="w-full rounded-md" controls width="100%" height="100%">
-              <source src="/assets/how-work.mp4" type="video/mp4" />
+              <source src="" type="video/mp4" />
               Your browser does not support the video tag.
             </video>
           </div>
